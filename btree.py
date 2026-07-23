259bafc79102f472b74586e826d1ebd2cbfb670a
CarapanceanuAlex <147550122+CarapanceanuAlex@users.noreply.github.com>
2026-07-23 16:38:43 +0300

    added the searching function of the BTree

--- btree.py
+++ btree.py
@@ -8,3 +8,17 @@ class BTree:
     def __init__(self,maxkeys = 3):
         self.root = BTreeNode()
         self.maxkeys = maxkeys
+
+    def search_key(self, key):
+        return self._search_node(self.root, key)
+
+    def _search_node(self, node, key):
+        for i in range(len(node.keys)):
+            if node.keys[i] == key:
+                return node.keys[i]
+            elif key < node.keys[i]:
+                return self._search_node(node.children[i], key)
+        if node.isleaf == False:
+            return self._search_node(node.children[len(node.keys)], key)
+        elif node.isleaf == True:
+            return None
